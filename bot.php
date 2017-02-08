0347addfcb4dd161594fe9380d6b3464f7c4515f
MARD0NA <c9mohammed.122@gmail.com>
2017-02-09 01:47:08 +0300

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,6 +1,6 @@
 <?php
 
-define('BOT_TOKEN', '149712985:AAHKHyBL1UeJAw0q2E2lH0p4MIBVjZgWjO4');
+define('BOT_TOKEN', '249487889:AAHvWLef3KgBH_xDagh9t7EuLf-sXZXs9zk');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
